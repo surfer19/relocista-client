4e9b6418727c0d5f0294aa0977e68aa302dd263e
Marian <marianmrva123@gmail.com>
2020-05-21 11:37:46 +0200

    Add map component with selectedPlaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';   // agm-direction
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatChipsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCGZlSNroO0PnMks0YWviJFtiX_SEtmS1o',
+      libraries: ['places']
+    }),
+    AgmDirectionModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+declare var google: any;
+
+@Component({
+  selector: 'map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+
+export class MapComponent implements OnInit {
+  // current selected point
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  formattedAddress: string;
+  foundName: string;
+  // list of selected points
+  selectedPlaces: Array<any>;
+  private geoCoder;
+
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 15;
+      });
+    }
+  }
+
+  // markerDragEnd($event: MouseEvent) {
+  //   console.log($event);
+  //   this.latitude = $event.coords.lat;
+  //   this.longitude = $event.coords.lng;
+  //   this.getAddress(this.latitude, this.longitude);
+  //   // console.log('foundAddress', foundAddress);
+  // }
+
+  private getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ location: { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+          console.log('get this.address', this.address);
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
+  public onAddNewPlace(e) {
+    this.selectedPlaces.push({
+      formattedAddress: this.formattedAddress,
+      name: this.foundName,
+      latitude: this.latitude,
+      longitude: this.longitude
+    });
+    console.log('add new place', this.selectedPlaces);
+  }
+
+  ngOnInit() {
+    this.selectedPlaces = [];
+    this.setCurrentLocation();
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      // this.setCurrentLocation();
+      // this.geoCoder = new google.maps.Geocoder();
+
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          // get the place result
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          console.log('place', place);
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          this.formattedAddress = place.formatted_address;
+          this.foundName = place.name;
+          // set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 16;
+        });
+      });
+    });
+  }
+}
